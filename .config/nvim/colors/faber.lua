2bcf343a3ca9cebd04fdfa221cae384ef9848a63
aarondiel <aaron.diel@t-online.de>
2021-09-07 19:01:41 +0200

    changed layout of colorscheme

--- .config/nvim/colors/faber.lua
+++ .config/nvim/colors/faber.lua
@@ -1,2 +1,13 @@
 -- see highlight-args E416 E417 E423
-vim.highlight.create('String', { ctermbg = 'yellow', guibg = vim.NONE }, false)
+-- cterm: bold underline undercurl strikethrough reverse inverse italic standout nocombine NONE
+
+local highlight_groups = {
+	{ 'String', { cterm='italic', ctermfg = '9' } }
+}
+
+for _, highlight_group in pairs(highlight_groups) do
+	local group_name = highlight_group[1]
+	local group_options = highlight_group[2]
+
+	vim.highlight.create(group_name, group_options, false)
+end
