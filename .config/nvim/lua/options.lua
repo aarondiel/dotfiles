aecad9b776ae61d72ef47bad5ee62e7a5f439489
aarondiel <aaron.diel@t-online.de>
2021-10-09 23:54:18 +0200

    adapting to nvim-tree changes

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -25,51 +25,66 @@ vim.opt.shortmess:append({ c = true })
 vim.opt.completeopt = 'menuone,noinsert,noselect'
 vim.opt.signcolumn = 'number'
 
-vim.g.nvim_tree_quit_on_open = true
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_group_empty = true
-vim.g.nvim_tree_disable_window_picker = true
+local nvim_tree_present, nvim_tree = pcall(require, 'nvim-tree')
+if nvim_tree_present then
+	vim.g.nvim_tree_quit_on_open = 1
+	vim.g.nvim_tree_highlight_opened_files = 1
+	vim.g.nvim_tree_group_empty = 1
+	vim.g.nvim_tree_disable_window_picker = 1
 
-local tree_cb = require('nvim-tree.config').nvim_tree_callback
-require('nvim-tree').setup {
-	auto_close = true,
-	update_cwd = true,
-	disable_default_keybindings = 1,
-	bindings = {
-		{ key = { '<cr>', 'l' }, cb = tree_cb('edit') },
-		{ key = 'cd', cb = tree_cb('cd') },
-		{ key = '<c-v>', cb = tree_cb('vsplit') },
-		{ key = '<c-x>', cb = tree_cb('split') },
-		{ key = '<c-t>', cb = tree_cb('tabnew') },
-		{ key = '<', cb = tree_cb('prev_sibling') },
-		{ key = '>', cb = tree_cb('next_sibling') },
-		{ key = 'P', cb = tree_cb('parent_node') },
-		{ key = '<bs>', cb = tree_cb('close_node') },
-		{ key = '<s-cr>', cb = tree_cb('close_node') },
-		{ key = '<tab>', cb = tree_cb('preview') },
-		{ key = 'K', cb = tree_cb('first_sibling') },
-		{ key = 'J', cb = tree_cb('last_sibling') },
-		{ key = 'I', cb = tree_cb('toggle_ignored') },
-		{ key = 'H', cb = tree_cb('toggle_dotfiles') },
-		{ key = 'R', cb = tree_cb('refresh') },
-		{ key = 'a', cb = tree_cb('create') },
-		{ key = 'd', cb = tree_cb('remove') },
-		{ key = 'r', cb = tree_cb('rename') },
-		{ key = '<c-r>', cb = tree_cb('full_rename') },
-		{ key = 'x', cb = tree_cb('cut') },
-		{ key = 'yy', cb = tree_cb('copy') },
-		{ key = 'p', cb = tree_cb('paste') },
-		{ key = 'y', cb = tree_cb('copy_name') },
-		{ key = 'Y', cb = tree_cb('copy_path') },
-		{ key = 'gy', cb = tree_cb('copy_absolute_path') },
-		{ key = '[c', cb = tree_cb('prev_git_item') },
-		{ key = ']c', cb = tree_cb('next_git_item') },
-		{ key = '-', cb = tree_cb('dir_up') },
-		{ key = 's', cb = tree_cb('system_open') },
-		{ key = 'q', cb = tree_cb('close') },
-		{ key = 'g?', cb = tree_cb('toggle_help') }
-	}
-}
+	local nvim_tree_mappings = {}
 
-vim.cmd('autocmd BufEnter * if (isdirectory(expand("%"))) | execute("cd expand("%")") | bd | execute("NvimTreeFocus") | endif')
+	local nvim_tree_config_present, nvim_tree_config = pcall(require, 'nvim-tree.config')
+	if nvim_tree_config_present then
+		local tree_cb = nvim_tree_config.nvim_tree_callback
+
+		nvim_tree_mappings = {
+			{ key = { '<cr>', 'l' }, cb = tree_cb('edit') },
+			{ key = 'cd', cb = tree_cb('cd') },
+			{ key = '<c-v>', cb = tree_cb('vsplit') },
+			{ key = '<c-x>', cb = tree_cb('split') },
+			{ key = '<c-t>', cb = tree_cb('tabnew') },
+			{ key = '<', cb = tree_cb('prev_sibling') },
+			{ key = '>', cb = tree_cb('next_sibling') },
+			{ key = 'P', cb = tree_cb('parent_node') },
+			{ key = '<bs>', cb = tree_cb('close_node') },
+			{ key = '<s-cr>', cb = tree_cb('close_node') },
+			{ key = '<tab>', cb = tree_cb('preview') },
+			{ key = 'K', cb = tree_cb('first_sibling') },
+			{ key = 'J', cb = tree_cb('last_sibling') },
+			{ key = 'I', cb = tree_cb('toggle_ignored') },
+			{ key = 'H', cb = tree_cb('toggle_dotfiles') },
+			{ key = 'R', cb = tree_cb('refresh') },
+			{ key = 'a', cb = tree_cb('create') },
+			{ key = 'd', cb = tree_cb('remove') },
+			{ key = 'r', cb = tree_cb('rename') },
+			{ key = '<c-r>', cb = tree_cb('full_rename') },
+			{ key = 'x', cb = tree_cb('cut') },
+			{ key = 'yy', cb = tree_cb('copy') },
+			{ key = 'p', cb = tree_cb('paste') },
+			{ key = 'y', cb = tree_cb('copy_name') },
+			{ key = 'Y', cb = tree_cb('copy_path') },
+			{ key = 'gy', cb = tree_cb('copy_absolute_path') },
+			{ key = '[c', cb = tree_cb('prev_git_item') },
+			{ key = ']c', cb = tree_cb('next_git_item') },
+			{ key = '-', cb = tree_cb('dir_up') },
+			{ key = 's', cb = tree_cb('system_open') },
+			{ key = 'q', cb = tree_cb('close') },
+			{ key = 'g?', cb = tree_cb('toggle_help') }
+		}
+	end
+
+	nvim_tree.setup({
+		hijack_cursor = true,
+		auto_close = true,
+		update_cwd = true,
+		view = {
+			mappings = {
+				custom_only = vim.fn.empty(nvim_tree_mappings) == 0,
+				list = nvim_tree_mappings
+			}
+		}
+	})
+end
+
 vim.cmd('autocmd FileType text setlocal spell spelllang=en')
