dbb6f5d0539905db7412b2625042d6dc5189e351
aarondiel <aaron.diel@t-online.de>
2022-08-29 09:53:43 +0200

    feat(new nvim mappings)

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,10 +8,13 @@ utils.keymaps({
 	{ "n", "<leader>k", "<cmd>wincmd k<cr>" },
 
 	{ "n", "Y", "y$" },
+	{ "n", "<leader>m", "<cmd>messages<cr>" },
 	{ "v", "<leader>y", "\"+y" },
 	{ "n", "<leader>i", reindent_pasted_text },
 	{ "n", "<leader>p", "\"+p" .. reindent_pasted_text },
 	{ { "n", "v" }, "p", "p" .. reindent_pasted_text },
 
-	{ "n", "<esc>", "<cmd>nohlsearch<cr>" }
+	{ "n", "<esc>", "<cmd>nohlsearch<cr>" },
+
+	{ "n", "<leader>tt", "<ctrl-w>T" }
 })
