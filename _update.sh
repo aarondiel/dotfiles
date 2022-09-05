5264f244d255bbed8c26983b62f64f90a8896873
aarondiel <aaron.diel@t-online.de>
2022-09-05 14:19:10 +0200

    feat(completion script for update.sh)

---
+++ _update.sh
@@ -0,0 +1,12 @@
+#compdef update.sh
+
+describe_configs() {
+	_values -s "," "configs" "nvim" "zsh"
+}
+
+_arguments \
+	"-h[show help message]" \
+	"--help[show help message]" \
+	"--configs[list available configs]" \
+	"-o[only install certain configs]:configs:configs:describe_configs" \
+	"--only[only install certain configs]:configs:describe_configs"
