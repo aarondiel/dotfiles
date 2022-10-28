b7132a992cda99b623a13b035e06ca31a68a1bd1
aarondiel <aaron.diel@t-online.de>
2022-10-28 10:20:56 +0200

    feat: better opening script

--- lf/scripts/open.sh
+++ lf/scripts/open.sh
@@ -3,23 +3,57 @@ set -Cef
 files="$1"
 num_files="$(echo "$files" | wc --lines)"
 
+get_mime_index() {
+	mime_type="$1"
+
+	case "$mime_type" in
+		text/* | inode/x-empty) echo "text";;
+		image/*) echo "image";;
+		application/pdf) echo "pdf";;
+		image/x-xcf) echo "gimp";;
+	esac
+}
+
 single_file() {
 	file=$(readlink -f "$1")
 	mime_type=$(file --brief --mime-type --dereference "$file")
 
-	case "$mime_type" in
-		text/* | inode/x-empty)
-			$EDITOR "$file"
-			;;
-
-		image/*)
-			feh "$file"
-			;;
+	case $(get_mime_index "$mime_type") in
+		text) $EDITOR "$file";;
+		image) setsid -f feh "$file" > /dev/null;;
+		pdf) setsid -f evince "$file" > /dev/null;;
+		gimp) setsid -f gimp "$file" > /dev/null;;
 	esac
 }
 
 multiple_files() {
-	:
+	set -- $files
+
+	first_mime_index=""
+	dereferenced_files=""
+
+	while [ -n "$1" ]
+	do
+		file=$(readlink -f "$1")
+		mime_type=$(file --brief --mime-type --dereference "$file")
+		mime_index=$(get_mime_index "$mime_type")
+		shift 1
+
+		[ -z "$first_mime_index" ] &&
+			first_mime_index="$mime_index" &&
+			continue
+
+		[ "$mime_index" != "$first_mime_index" ] &&
+			return 0
+
+		[ -z "$dereferenced_files" ] &&
+			dereferenced_files=$(echo -e "${file}\n") ||
+			dereferenced_files=$(echo -e "${dereferenced_files}\n${file}\n")
+	done
+
+	case "$first_mime_index" in
+		image) setsid -f feh "$dereferenced_files" > /dev/null;;
+	esac
 }
 
 [ "$num_files" = "1" ] &&
