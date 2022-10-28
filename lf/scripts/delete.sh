343554ef910c3400c77ab4c3fa823bec911d4a17
aarondiel <aaron.diel@t-online.de>
2022-10-28 13:25:33 +0200

    fix: delete.sh

--- lf/scripts/delete.sh
+++ lf/scripts/delete.sh
@@ -1,15 +1,24 @@
 set -Cef
 
+IFS=$(echo -e '\n+')
+IFS=${IFS%?}
+
 files="$1"
 
 generate_prompt_text() {
 	num_files="$(echo "$files" | wc --lines)"
 	[ "$num_files" = "1" ] &&
-		echo "do you want to delete this file?" >&1 ||
-		echo "do you want to delete ${num_files} files?" >&1
+		echo "do you want to delete this file?" ||
+		echo "do you want to delete ${num_files} files?"
+}
+
+delete_files() {
+	for file in $files
+	do
+		rm -rf "$file"
+	done
 }
 
 prompt=$(generate_prompt_text)
 
-gum confirm "$prompt" &&
-	rm -rf $files
+gum confirm "$prompt" && delete_files
